f382d09bf10ba31b7573532654daba964697ce13
shashwat <shashwat0313@yahoo.co.in>
2024-05-28 23:07:37 +0530

    added loading icon to postdetails

--- client/memories-yt/src/components/PostDetails/PostDetails.js
+++ client/memories-yt/src/components/PostDetails/PostDetails.js
@@ -11,47 +11,45 @@ import { getPost } from "../../actions/posts";
 
 const PostDetails = () => {
 
-    console.log("Post Details");
+  console.log("Post Details");
 
-    const post = useSelector((state)=>{
-      console.log("state( postdetails ):", state.posts);
-      return state.posts?.post
-    })
+  const post = useSelector((state) => {
+    console.log("state( postdetails ):", state.posts);
+    return state.posts?.post
+  })
 
-    const {id} = useParams()
-    const history = useHistory()
-    const classes = useStyles()
-    const dispatch = useDispatch()
+  const { id } = useParams()
+  const history = useHistory()
+  const classes = useStyles()
+  const dispatch = useDispatch()
 
-    useEffect(()=>{
-      dispatch(getPost(id))
-      console.log("post in postdetails:", post);
-    }, [id])
+  useEffect(() => {
+    dispatch(getPost(id))
+    console.log("post in postdetails:", post);
+  }, [id])
 
-    return (
-      // <>hello</>
+  return (
 
-      (
-        post &&
+    post ? (
       <div className={classes.card}>
-      <div className={classes.section}>
-        <Typography variant="h3" component="h2">{post.title}</Typography>
-        <Typography gutterBottom variant="h6" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
-        <Typography gutterBottom variant="body1" component="p">{post.message}</Typography>
-        <Typography variant="h6">Created by: {post.name}</Typography>
-        <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
-        <Divider style={{ margin: '20px 0' }} />
-        <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
-        <Divider style={{ margin: '20px 0' }} />
-        <Typography variant="body1"><strong>Comments - coming soon!</strong></Typography>
-        <Divider style={{ margin: '20px 0' }} />
+        <div className={classes.section}>
+          <Typography variant="h3" component="h2">{post.title}</Typography>
+          <Typography gutterBottom variant="h6" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+          <Typography gutterBottom variant="body1" component="p">{post.message}</Typography>
+          <Typography variant="h6">Created by: {post.name}</Typography>
+          <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
+          <Divider style={{ margin: '20px 0' }} />
+          <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
+          <Divider style={{ margin: '20px 0' }} />
+          <Typography variant="body1"><strong>Comments - coming soon!</strong></Typography>
+          <Divider style={{ margin: '20px 0' }} />
+        </div>
+        <div className={classes.imageSection}>
+          <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
+        </div>
       </div>
-      <div className={classes.imageSection}>
-        <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
-      </div>
-    </div>
-      )
 
+    ) : <CircularProgress />
   )
 }
 
