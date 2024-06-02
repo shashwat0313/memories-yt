821f56e4fd758a3b31c37ca64c1cc827c81a182f
shashwat <shashwat0313@yahoo.co.in>
2024-06-02 16:03:16 +0530

    ...

--- client/memories-yt/index.js
+++ client/memories-yt/index.js
@@ -6,7 +6,7 @@ app.use('/static', express.static(path.join(__dirname, '/build/static')));
 
 console.log(path.join(__dirname, 'static'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/build/index.html'));
 })
 app.listen(process.env.PORT || 3000, () => {
